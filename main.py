abb05bdbe8000399a8e72eea44040c479856ee52
Michal Plebanski <michal.plebanski@golem.network>
2018-11-20 10:33:07 +0100

    Added typing to ease the development

--- main.py
+++ main.py
@@ -4,15 +4,17 @@ from autobahn.asyncio.wamp import ApplicationSession
 from twisted.internet._sslverify import optionsForClientTLS  # noqa # pylint: disable=protected-access
 from twisted.internet import reactor
 from twisted.internet.endpoints import SSL4ClientEndpoint
+from twisted.internet.defer import inlineCallbacks, Deferred
 
 import ssl
 
-
+# Hardcoded golem node_A cert 
 cert_path = '/home/mplebanski/Projects/golem/node_A/rinkeby/crossbar/rpc_cert.pem'
 
 with open(cert_path, 'rb') as f:
     cert_data = f.read()
 
+# Mismatch golem.local - localhost
 ssl.match_hostname = lambda cert, hostname: True
 context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
 context.load_verify_locations(cert_path)
@@ -28,6 +30,9 @@ comp = Component(
                 "type": "tcp",
                 "port": 61000,
                 "tls": context
+            },
+            "options": {
+                "open_handshake_timeout": 3,
             }
         }
     ],
@@ -41,8 +46,38 @@ comp = Component(
     realm=u"golem",
 )
 
+from autobahn.asyncio.websocket import WampWebSocketClientProtocol
+from autobahn.asyncio.wamp import Session
+from autobahn.wamp.types import SessionDetails
+
 @comp.on_join
-async def joined(session, details):
+async def joined(session: Session, details: SessionDetails):
+    await session.subscribe(lambda response: print('status ' + str(response)),
+                            u'evt.golem.status')
+    await session.subscribe(lambda response: print('net_connection ' + str(response)),
+                            u'evt.net.connection')
+    await session.subscribe(lambda response: print('task status ' + str(response)),
+                            u'evt.comp.task.status')
+    await session.subscribe(lambda response: print('subtask status ' + str(response)),
+                            u'evt.comp.subtask.status')
+    await session.subscribe(lambda response: print('task.prov_rejected ' + str(response)),
+                            u'evt.comp.task.prov_rejected')
+
+@comp.on_connect
+async def connected(session: Session, client_protocol: WampWebSocketClientProtocol):
+    pass
+
+@comp.on_disconnect
+async def disconnected(session: Session, details=None, was_clean=True):
+    pass
+
+@comp.on_ready
+async def ready(session: Session, details=None):
+    pass
+
+@comp.on_leave
+async def leave(session, details):
+    pass
 
 if __name__ == "__main__":
     run([comp])

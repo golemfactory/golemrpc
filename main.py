7ddbbe950436d736817d29c2d80fef5e3f718bd0
Michal Plebanski <michal.plebanski@golem.network>
2018-11-20 11:18:23 +0100

    Add timer and code clean up

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from autobahn.asyncio.component import Component
-from autobahn.asyncio.component import run
 from autobahn.asyncio.wamp import ApplicationSession
 from twisted.internet._sslverify import optionsForClientTLS  # noqa # pylint: disable=protected-access
 from twisted.internet import reactor
 from twisted.internet.endpoints import SSL4ClientEndpoint
 from twisted.internet.defer import inlineCallbacks, Deferred
 
+import asyncio
+from functools import partial
+import signal
 import ssl
+import txaio
+
+txaio.use_asyncio()  # noqa
 
 # Hardcoded golem node_A cert 
 cert_path = '/home/mplebanski/Projects/golem/node_A/rinkeby/crossbar/rpc_cert.pem'
@@ -78,6 +83,80 @@ async def ready(session: Session, details=None):
 @comp.on_leave
 async def leave(session, details):
     pass
+
+def run(components, log_level='info'):
+    if log_level is not None:
+        txaio.start_logging(level=log_level)
+    loop = asyncio.get_event_loop()
+    log = txaio.make_logger()
+
+    # Code taken from autobahn.wamp.component
+    def reactor_add_components(reactor, components):
+        def component_success(comp, arg):
+            log.debug("Component '{c}' successfully completed: {arg}", c=comp, arg=arg)
+            return arg
+
+        def component_failure(comp, f):
+            log.error("Component '{c}' error: {msg}", c=comp, msg=txaio.failure_message(f))
+            log.debug("Component error: {tb}", tb=txaio.failure_format_traceback(f))
+            return None
+
+        def component_start(comp):
+            d = txaio.as_future(comp.start, reactor)
+            txaio.add_callbacks(
+                d,
+                partial(component_success, comp),
+                partial(component_failure, comp),
+            )
+            return d
+
+        done_d = txaio.gather([component_start(c) for c in components], consume_exceptions=False)
+
+        def all_done(arg):
+            log.debug("All components ended; stopping reactor")
+            if isinstance(arg, Failure):
+                log.error("Something went wrong: {log_failure}", failure=arg)
+            try:
+                reactor.stop()
+            except ReactorNotRunning:
+                pass
+        txaio.add_callbacks(done_d, all_done, all_done)
+        return done_d
+
+    def reactor_add_signal_handling(reactor):
+        @asyncio.coroutine
+        def exit():
+            return reactor.stop()
+
+        def nicely_exit(signal):
+            log.info("Shutting down due to {signal}", signal=signal)
+            for task in asyncio.Task.all_tasks():
+                task.cancel()
+            asyncio.ensure_future(exit())
+
+        reactor.add_signal_handler(signal.SIGINT, partial(nicely_exit, 'SIGINT'))
+        reactor.add_signal_handler(signal.SIGTERM, partial(nicely_exit, 'SIGTERM'))
+
+    reactor_add_signal_handling(loop)
+    reactor_add_components(loop, components)
+
+    async def timer():
+        while True:
+            await asyncio.sleep(1.0)
+            print('Hello world!') 
+
+    d = txaio.as_future(timer)
+    txaio.add_callbacks(
+        d, 
+        lambda success: print('success: ' + str(success)),
+        lambda error: print('error: ' + str(error)),
+    )
+
+    try:
+        loop.run_forever()
+    except asyncio.CancelledError:
+        pass
+    loop.close()
 
 if __name__ == "__main__":
     run([comp])

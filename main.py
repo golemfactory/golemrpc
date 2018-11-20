55e227546f9a704e6a276dced859ed6f112a5f9d
Michal Plebanski <michal.plebanski@golem.network>
2018-11-20 12:44:47 +0100

    Remove timer

--- main.py
+++ main.py
@@ -5,23 +5,11 @@ from wamp.base import wamp_register_components
 
 txaio.use_asyncio()  # noqa
 
-async def timer():
-    while True:
-        await asyncio.sleep(1.0)
-        print('Hello world!') 
-
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
 
     from wamp.client import component
     wamp_register_components(loop, [component])
-
-    d = txaio.as_future(timer)
-    txaio.add_callbacks(
-        d, 
-        lambda success: print('success: ' + str(success)),
-        lambda error: print('error: ' + str(error)),
-    )
 
     try:
         loop.run_forever()

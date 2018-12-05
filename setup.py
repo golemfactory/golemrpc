49639c0a020679bf6ab762c07a0ab1b812a0743d
Michal Plebanski <michal.plebanski@golem.network>
2018-12-05 13:06:21 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,17 @@
 from setuptools import setup
 
+import os
+
+libdir = os.path.dirname(os.path.realpath(__file__))
+req_path = os.path.join(
+      libdir,
+      'golemrpc',
+      'requirements.txt'
+)
+
+with open(req_path, 'r') as f:
+    install_requires = list(f.read().splitlines())
+
 setup(name='golemrpc',
       version='0.1',
       description='Golem RPC library',
@@ -8,4 +20,5 @@ setup(name='golemrpc',
       author_email='contact@golem.network',
       license='MIT',
       packages=['golemrpc'],
+      install_requires=install_requires,
       zip_safe=False)

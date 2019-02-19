b43e753fb7fbea50e487b10cc76be1805e8afbab
Michal Plebanski <michal.plebanski@golem.network>
2019-02-19 10:53:44 +0100

    Fix blender example

--- examples/blender.py
+++ examples/blender.py
@@ -1,57 +1,58 @@
-import os
 import logging
+import os
 import uuid
-from pathlib import Path
+from pathlib import Path, PurePath
 
 from golemrpc.rpccomponent import RPCComponent
 
 logging.basicConfig(level=logging.INFO)
 
-# Authenticate with aws golem node on 61000 port (default) using cli_secret
-# and rpc_cert specified
-rpc = RPCComponent(
-    host='35.158.100.160',
-    cli_secret='golemcli_aws.tck',
-    rpc_cert='rpc_cert_aws.pem'
-)
-
 # Example assumes 'bmw27_cpu.blend' has been placed in your home directory
-cube_blend_path = os.path.join(
-    str(Path.home()),
-    'bmw27_cpu.blend'
-)
+cube_blend_path = Path.home() / PurePath('bmw27_cpu.blend')
 
 blender_dict = {
     "type": "Blender",
     # Give some unique name to the task
-    "name": "{}".format(str(uuid.uuid1())[:16]),
+    "name": "{}".format(str(uuid.uuid1())[:8]),
     "timeout": "0:10:00",
     "subtask_timeout": "0:09:50",
     "subtasks_count": 1,
     "bid": 1.0,
     "resources": [
-        cube_blend_path
+        # Specify where the input can be found
+        cube_blend_path.as_posix()
     ],
     "options": {
-        "output_path": str(Path.home()),
+        "output_path": Path.home().as_posix(),
         "format": "PNG",
         "resolution": [
-            800,
-            600
+            400,
+            300
         ]
     }
 }
+# Golem default installation directory is where we obtain cli_secret and rpc_cert
+datadir = '{home}/Projects/golem/node_A/rinkeby'.format(home=Path.home())
+
+# Authenticate with localhost:61000 (default) golem node using cli_secret
+# and rpc_cert specified
+rpc = RPCComponent(
+    cli_secret='{datadir}/crossbar/secrets/golemcli.tck'.format(datadir=datadir),
+    rpc_cert='{datadir}/crossbar/rpc_cert.pem'.format(datadir=datadir)
+)
 
 rpc.start()
 
-results = rpc.post_wait({
+# Ignore TaskCreatedEvent
+_ = rpc.post_wait({
     'type': 'CreateTask',
     'task': blender_dict
-})['results']
+})
 
-print(results)
+response = rpc.poll(timeout=None)
+print(response['results'])
 
 # Tell RPCComponent to disconnect with remote Golem
-rpc.post_wait({
+c.post_wait({
     'type': 'Disconnect'
 })
